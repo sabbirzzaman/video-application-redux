ea576b5ad30298c0f76b3e5fbc0e35a3fb60b76d
sabbirzzaman <sztonmoy7@gmail.com>
2022-08-31 20:55:34 +0600

    react router setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-// import Home from "./pages/Home";
-import Home from "./pages/Home";
-import Video from "./pages/Video";
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
+import Home from './pages/Home';
+import Video from './pages/Video';
 
 function App() {
-    return <Home />;
+    return (
+        <>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/videos/:videoId" element={<Video />} />
+            </Routes>
+            <Footer />
+        </>
+    );
 }
 
 export default App;
